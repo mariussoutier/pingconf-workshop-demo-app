3346e1bca194b03face0eca6c32e5c9d79e8346d
Zoltan Balazs <balagez@gmail.com>
2014-01-15 16:48:42 +0100

    Update dependencies and add a stub controller

--- project/Build.scala
+++ project/Build.scala
@@ -2,19 +2,25 @@ import sbt._
 import Keys._
 import play.Project._
 
+import com.typesafe.sbt.SbtScalariform._
+import scalariform.formatter.preferences.{ FormattingPreferences, IndentWithTabs }
+
 object ApplicationBuild extends Build {
 
-    val appName         = "forms"
-    val appVersion      = "1.0"
+	val appName	 	= "pingconf-workshop"
+	val appVersion	= "1.0"
 
-    val appDependencies = Seq(
-  	 "com.etaty.rediscala" %% "rediscala" % "1.2"
-    )
+	val appDependencies = Seq(
+		"com.etaty.rediscala" %% "rediscala" % "1.3"
+	)
 
-   
-	
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-       resolvers += "rediscala" at "https://github.com/etaty/rediscala-mvn/raw/master/releases/"
-    )
+	val customSettings = Seq(
+		resolvers += "rediscala" at "https://github.com/etaty/rediscala-mvn/raw/master/releases/"
+	) ++ scalariformSettings ++ Seq(
+		// code formatting
+		ScalariformKeys.preferences := FormattingPreferences().
+			setPreference(IndentWithTabs, true))
 
+
+	val main = play.Project(appName, appVersion, appDependencies).settings(customSettings: _*)
 }
